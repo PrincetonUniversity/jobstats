22c02d74d6790b516b860ec15debcb3ca5835575
Josko Plazonic <plazonic@gmail.com>
2024-02-02 16:49:22 -0500

    Use also GPU UUIDs for tracking slurm jobid/user that allocated it. This matches nvidia_gpu_prometheus_exporter update and helps avoid errors for when GPU ordinal number differs from its minor number.

--- slurm/epilog.d/gpustats_helper.sh
+++ slurm/epilog.d/gpustats_helper.sh
@@ -3,5 +3,9 @@ DEST=/run/gpustat
 [ -e $DEST ] || mkdir -m 755 $DEST
 for i in ${GPU_DEVICE_ORDINAL//,/ } ${CUDA_VISIBLE_DEVICES//,/ }; do
   rm -f $DEST/$i
+  if [ "${i:0:3}" != "MIG" ]; then
+    UUID="`/usr/bin/nvidia-smi --query-gpu=uuid --format=noheader,csv -i $i`"
+    rm -f "$DEST/$UUID"
+  fi
 done
 exit 0
--- slurm/prolog.d/gpustats_helper.sh
+++ slurm/prolog.d/gpustats_helper.sh
@@ -3,5 +3,9 @@ DEST=/run/gpustat
 [ -e $DEST ] || mkdir -m 755 $DEST
 for i in ${GPU_DEVICE_ORDINAL//,/ } ${CUDA_VISIBLE_DEVICES//,/ }; do
   echo $SLURM_JOB_ID $SLURM_JOB_UID > $DEST/$i
+  if [ "${i:0:3}" != "MIG" ]; then
+    UUID="`/usr/bin/nvidia-smi --query-gpu=uuid --format=noheader,csv -i $i`"
+    echo $SLURM_JOB_ID $SLURM_JOB_UID > "$DEST/$UUID"
+  fi
 done
 exit 0
